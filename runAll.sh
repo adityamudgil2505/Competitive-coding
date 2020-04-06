d81a86f09413093395b368db5449d4d55873a1cd
am2505 <adityamudgil2505@gmail.com>
2020-04-06 22:54:19 +0530

    Added execution time feature

--- runAll.sh
+++ runAll.sh
@@ -15,7 +15,7 @@ g++ -std=c++17 solution.cpp
 [ ! -f "$compiledFile" ] && echo "\033[31mCompiler Error \033[0m" && exit || echo "\033[32mSuccessfully Complied \033[0m"
 
 # Checking if the question has test cases or not
-[ ! -f ./inp0.txt ] && echo "\033[93mThere is no input files \033[0m" && exit || echo "We have following input test cases file \n$(ls inp*.txt)"
+[ ! -f ./inp0.txt ] && echo "\033[93mThere is no input files \033[0m" && exit 
 
 # Grabing all the input test cases and start executing
 echo "\033[36mExecuting... \033[0m"
@@ -26,6 +26,7 @@ successfullyPassed=1
 # Running program on all test cases
 for(( i=0; i<${#inputFiles[@]}; i++ ))
 do
+  start=`gdate +%s.%N`
   gtimeout $timeoutVal ./a.out < inp$i.txt > yout$i.txt
 
   # Checking if program exceed given time interval or not
@@ -34,7 +35,9 @@ do
     echo "\033[31mTime out on test case $i. This might be runtime error or a slow program\033[0m"
     successfullyPassed=0
   else
-    echo "\033[36mSuccessfully run on testcase $i \033[0m"
+    end=`gdate +%s.%N`
+    runtime=$(echo "$end - $start" | bc)  
+    echo "\033[36mSuccessfully\033[0m run on \t\033[33mTestcase: $i \033[0m \t Time taken: $runtime seconds"
   fi
 done
 
--- runCustom.sh
+++ runCustom.sh
@@ -6,6 +6,7 @@ cd ./$1
 # Deleting previous executable file (if exists)
 echo "\033[93mDeleting a.out file if exist \033[0m"
 rm -rf $compiledFile
+rm -rf customOutput.txt
 
 # Compiling starts
 echo "\033[36mCompiling... \033[0m"
@@ -14,11 +15,14 @@ g++ -std=c++17 solution.cpp
 # Checking if successfully compilied or not
 [ ! -f "$compiledFile" ] && echo "\033[31mCompiler Error \033[0m" && exit || echo "\033[32mSuccessfully Complied \033[0m"
 
+start=`gdate +%s.%N`
 gtimeout $timeoutVal ./a.out < customInput.txt > customOutput.txt
 # Checking if program exceed given time interval or not
 if [ $? -eq 124 ]
 then
   echo "\033[31mTime out on custom input testcase. This might be runtime error or a slow program\033[0m"
 else
-  echo "\033[32mSuccessfully run on custom testcase $i \033[0m"
+  end=`gdate +%s.%N`
+  runtime=$(echo "$end - $start" | bc)  
+  echo "\033[36mSuccessfully\033[0m run on \t\033[33mCustom Testcase \033[0m \t Time taken: $runtime seconds"
 fi
