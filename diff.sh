4dd1ed0a9933d155378050801c8b8cc8cf30972f
am2505 <adityamudgil2505@gmail.com>
2020-04-07 01:07:51 +0530

    Added diff comparision file

--- diff.sh
+++ diff.sh
@@ -1,7 +1,5 @@
 cd $1
-echo $(pwd)
 mainoutfiles=(`ls out*.txt`)
-
 yourfiles=(`ls yout*.txt`)
 
 cmd="vim -c 'set diffopt=filler,vertical' -c 'edit ${mainoutfiles[0]}' -c 'diffsplit ${yourfiles[0]}' "
@@ -9,7 +7,5 @@ len=${#mainoutfiles[@]}
 for((i=1; i<$len; i++)) do
   cmd="${cmd} -c 'tabe ${mainoutfiles[i]}' -c 'diffsplit ${yourfiles[i]}' "
 done
-
-echo $cmd
 eval $cmd
 cd ..
